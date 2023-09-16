8d7379aff895f39b88504eeec85421c2aa4454e9
Valentin Purrucker <vali.pur13@gmail.com>
2023-09-16 20:48:33 +0200

    Added WiFi Manager component - manage wifi connection automatically (auto. reconnect/restart) - add connection state callbacks to be used for further scheduling - add wifi-name/password as build flag in ini

---
+++ include/WiFiManager.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+class WifiManager
+{
+
+    using OnConnectedCallback = std::function<void()>;
+
+public:
+    enum WiFiState
+    {
+        Uninitialized,
+        Connected,
+        Disconnected,
+        Connecting,
+        Timeout,
+        Failed
+    };
+
+    WifiManager();
+
+    void setup();
+
+    void loop();
+
+    bool isConnected();
+
+    bool addConnectionHandler(OnConnectedCallback onConnected);
+
+    OnConnectedCallback mOnConnectedHandlers[10];
+
+private:
+    void connectWiFi();
+
+    void onDisconnect(const WiFiEventStationModeDisconnected &event);
+
+    void onGotIP(const WiFiEventStationModeGotIP &event);
+
+    void checkTimeout();
+
+    void reconnect();
+
+    WiFiClient mClient;
+
+    WiFiState mCurrentState = WiFiState::Uninitialized;
+
+    WiFiEventHandler mConnectedHandler;
+    WiFiEventHandler mDisconnectedHandler;
+
+    int mLastConnectionAttemptTs = 0;
+
+    int mReconnectCounter = 0;
+};
