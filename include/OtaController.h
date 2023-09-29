88525725c5e050068c2477d234759c0dfa1c759a
Valentin Purrucker <vali.pur13@gmail.com>
2023-09-29 17:44:58 +0200

    Added OtaController component - update esp over the air (ota) - add simple async http server to test esp connection

---
+++ include/OtaController.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Arduino.h>
+#include <ArduinoOTA.h>
+
+class OtaController
+{
+public:
+    enum OtaControllerState
+    {
+        Uninitialized,
+        Setup
+    };
+
+    void
+    setup();
+
+    void loop();
+
+private:
+    void handleOtaStart();
+    void handleOtaProgress(size_t current, size_t final);
+    void handleOtaEnd();
+
+    OtaControllerState mCurrentState{Uninitialized};
+};
---
+++ src/OtaController.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include "OtaController.h"
+
+AsyncWebServer server{80};
+
+// PUBLIC:
+void OtaController::setup()
+{
+    ArduinoOTA.begin(false);
+    ArduinoOTA.onStart(std::bind(&OtaController::handleOtaStart, this));
+    ArduinoOTA.onEnd(std::bind(&OtaController::handleOtaEnd, this));
+
+    server.begin();
+    server.on("/status", [](AsyncWebServerRequest *req)
+              { req->send(200, "text/html", "ESP Online"); });
+
+    mCurrentState = Setup;
+}
+
+void OtaController::loop()
+{
+    switch (mCurrentState)
+    {
+    case Uninitialized:
+        return;
+    case Setup:
+        ArduinoOTA.handle();
+        break;
+    }
+}
+
+// PRIVATE:
+void OtaController::handleOtaStart()
+{
+    // TODO: Handle ota start
+}
+
+void OtaController::handleOtaProgress(size_t current, size_t final)
+{
+    // TODO: Handle ota progress
+}
+
+void OtaController::handleOtaEnd()
+{
+    // TODO: Handle ota end
+}
